1bdc4a9e9d6e56c487f1c28b0faafefe721b8a20
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-12-25 18:14:23 +0300

    feat(dbt): staging model

--- dbt/finhouse/models/staging/transactions/stg_transaction_types.sql
+++ dbt/finhouse/models/staging/transactions/stg_transaction_types.sql
@@ -1,9 +1,8 @@
 {{
     config(
-        materialized='incremental',
-        unique_key='transaction_id',
-        incremental_strategy='merge',
-        tags=['daily']
+        materialized='table',
+        unique_key='transaction_type_id',
+        tags=['weekly']
     )
 }}
 
@@ -11,12 +10,12 @@ SELECT
     TRY_TO_TIMESTAMP(date_created) AS date_created,
     TRY_TO_TIMESTAMP(date_updated) AS date_updated,
     active,
-    transaction_id,
-    amount,
-    description,
-    account_id,
-    transaction_type_id
-FROM microhouse_raw.public.transactions 
+    transaction_type_id,
+    name,
+    code,
+    direction,
+    description 
+FROM microhouse_raw.public.transaction_types
 {% if is_incremental() %}
     WHERE TRY_TO_TIMESTAMP(date_updated) > (SELECT MAX(date_updated) FROM {{ this }})
 {% endif %}
--- dbt/finhouse/models/staging/transactions/stg_transfers.sql
+++ dbt/finhouse/models/staging/transactions/stg_transfers.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-        unique_key='transaction_id',
+        unique_key='transfer_id',
         incremental_strategy='merge',
         tags=['daily']
     )
@@ -11,12 +11,13 @@ SELECT
     TRY_TO_TIMESTAMP(date_created) AS date_created,
     TRY_TO_TIMESTAMP(date_updated) AS date_updated,
     active,
-    transaction_id,
+    transfer_id,
     amount,
-    description,
-    account_id,
-    transaction_type_id
-FROM microhouse_raw.public.transactions 
+    from_account_id,
+    reference_id,
+    sender_transaction_id
+    to_account_id
+FROM microhouse_raw.public.transfers
 {% if is_incremental() %}
     WHERE TRY_TO_TIMESTAMP(date_updated) > (SELECT MAX(date_updated) FROM {{ this }})
 {% endif %}
